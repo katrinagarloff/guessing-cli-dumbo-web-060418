c50e84e3b1e4c644c631c3a5b4f86fd778c71bb3
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-28 19:11:47 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,6 +3,8 @@ def run_guessing_game
   puts "Guess a number between 1 and 6"
   guess = gets.chomp
   do loop
+  puts "Guess a number between 1 and 6"
+  guess = gets.chomp
   
   break if guess == "exit"
 end
